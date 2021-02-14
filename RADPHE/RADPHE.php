626fc2f995836a03fd64b8d4839fc728abe1f0aa
Kelly G. Allen <KellyGAllen@GMail.com>
2021-02-14 11:13:50 -0800

    cleanup.

---
+++ RADPHE/RADPHE.php
@@ -0,0 +1,9 @@
+<?php
+//Fallback Hook, SHOULD BE .htaccess install in Apache AllowOveride All
+@require_once($_SERVER['DOCUMENT_ROOT'].'/_system/_SiteEngine.php');
+//How this page is differant in content.
+echo 'The Point to something.';
+//EXIT of request.
+exit('All Done.');
+//Thats it!
+?>

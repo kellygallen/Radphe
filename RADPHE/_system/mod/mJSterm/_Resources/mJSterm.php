178e2840eff0ba5d6385dcd664620a81c867d514
Kelly G. Allen <KellyGAllen@GMail.com>
2021-02-13 19:30:49 -0800

    Pretty code. not to clutter single pass mcv. moved functions to function file.

--- RADPHE/_system/mod/mJSterm/_Resources/mJSterm.php
+++ RADPHE/_system/mod/mJSterm/_Resources/mJSterm.php
@@ -5,78 +5,9 @@ if (session_status() == PHP_SESSION_NONE) session_start();
 session_write_close();
 $link = @mysqli_connect("localhost","mod_mJSterm","","radphe");
 if (!$link) $link = @mysqli_connect("dev.localhost","user","password","db");
-//include(__DIR__.'/../_DB_SESSIONS.php');
 CMS_Skinner::$Page['LayoutFile']='WP';
+include($_SERVER['DOCUMENT_ROOT'].'/_system/mod/mJSterm/functions.php');
 
-function is_func_enabled($func) {
-	$disabled = explode(',', ini_get('disable_functions'));
-	foreach ($disabled as $disableFunction) {
-		$is_disabled[] = trim($disableFunction);
-	}
-	if ((in_array($func,$is_disabled))&&(function_exists($func))) {
-		return false;
-	} else {
-		return true;
-	}
-	return false;
-}
-function skippy_sleep($timeout=1) {
-	$Start = time();
-	$StartPart = explode('.',$Start);
-	$finishStart=$StartPart[0]+$timeout;
-	$finishStart=$finishStart.'.'.$StartPart[1];
-	$finishStart=$Start+$timeout;
-	
-	$MEthod=false;
-	if ((1)&&(is_func_enabled('sleep'))) {
-		$MEthod='sleep';
-		@sleep($timeout);
-	} elseif ((1)&&(is_func_enabled('usleep'))) {
-		$MEthod='usleep';
-		@usleep(1000000*$timeout);
-	} elseif ((1)&&(is_func_enabled('curl_exec'))) {
-		$MEthod='curl';
-		$ch = curl_init('http://10.200.100.20/No_Sleep_Makes_NGINX_Posing_As_Apache_To_Do_Weird_Things');
-		//localhost or a site that putposly never flushes output and keeps a iniated but null connection open for atleast a few seconds. or just a slow to respond site like my wordpress.
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_VERBOSE, 0);    
-		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);//maybe+1
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-		$response = curl_exec($ch);
-		if ($response === false) {
-			$info = curl_getinfo($ch);
-			if ($info['http_code'] === 0) {
-				// timeouth=
-			}
-		}
-	} elseif (0) {//BURN IT UP ================================= 0 = not.
-		//This one gets cpu hot.
-//microtime is slower. fix use it explode by space and make its time boundery use the unix ts plus the mtime for the measure. if a resonable delay was made allow it to recall it self.
-//		$MEthod='microtime';
-		$MEthod='time';
-		$end = time();
-//		$diff=round($end,2)-round($Start,2);
-		$diff=$end-$Start;
-//		die('sleepy:'.$end.'-'.$Start.'='.$diff.'>?'.$timeout.' But the fiish is '.$finishStart);
-//		if( floatval( (string) $a ) < floatval( (string) $b) ) { //do something }
-//		$diff=bcsub($end, $Start, 4); //nobcmath
-		while(
-				($diff < $timeout)
-			) {
-//			error_log('sleepy:'.$end.'-'.$Start.'='.$diff.'>?'.$timeout.' But the fiish is '.$finishStart,0);
-			$end = time();
-//			$diff=round($end,2)-round($Start,2);
-			$diff=$end-$Start;
-			continue;
-		}
-	} else {//fail.
-		return array('ME'=>false);
-	}
-	$end = time();
-//	while (($end-$Start)<$timeout) skippy_sleep(1);
-	$diff=$end-$Start;
-	return array('Begin'=>$Start,'End'=>$end,'ME'=>$MEthod,'Diff'=>$diff);
-}
 //View,rendor
 if (isset($_GET['GUI'])) {
 	if ($_GET['GUI']==0) {
@@ -156,8 +87,7 @@ function attachFormSubmitEvent(formId){
 }
 </script>
 <h3>For now just 20 frames.</h3>
-<pre>Work aroound needded for free hosting. it allows no sleep() and no set_time_limit(). So i will have to be tricky telling time and juggleing living handlers. ... or say forget free hosting!
-Its currently Dumb. Please disconnect before you leave... and I am setting to run for 20 seconds max.
+<pre>I am setting to run for 20 seconds max.
 So you may need to press connect to get it going even though it shows connected.</pre>
 <form id="Term" target="TermResponce" action="mJSterm.php" method="post" name="mJSterm" enctype="multipart/form-data"  onsubmit="formSubmit(event);return false">
 	<fieldset>
---
+++ RADPHE/_system/mod/mJSterm/functions.php
@@ -0,0 +1,73 @@
+<?php
+@require_once($_SERVER['DOCUMENT_ROOT'].'/_system/_SiteEngine.php');//Fallback Hook.
+
+function is_func_enabled($func) {
+    $disabled = explode(',', ini_get('disable_functions'));
+    foreach ($disabled as $disableFunction) {
+        $is_disabled[] = trim($disableFunction);
+    }
+    if ((in_array($func,$is_disabled))&&(function_exists($func))) {
+        return false;
+    } else {
+        return true;
+    }
+    return false;
+}
+function skippy_sleep($timeout=1) {
+    $Start = time();
+    $StartPart = explode('.',$Start);
+    $finishStart=$StartPart[0]+$timeout;
+    $finishStart=$finishStart.'.'.$StartPart[1];
+    $finishStart=$Start+$timeout;
+    
+    $MEthod=false;
+    if ((1)&&(is_func_enabled('sleep'))) {
+        $MEthod='sleep';
+        @sleep($timeout);
+    } elseif ((1)&&(is_func_enabled('usleep'))) {
+        $MEthod='usleep';
+        @usleep(1000000*$timeout);
+    } elseif ((1)&&(is_func_enabled('curl_exec'))) {
+        $MEthod='curl';
+        $ch = curl_init('http://10.200.100.20/No_Sleep_Makes_NGINX_Posing_As_Apache_To_Do_Weird_Things');
+        //localhost or a site that putposly never flushes output and keeps a iniated but null connection open for atleast a few seconds. or just a slow to respond site like my wordpress.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_VERBOSE, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);//maybe+1
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+        $response = curl_exec($ch);
+        if ($response === false) {
+            $info = curl_getinfo($ch);
+            if ($info['http_code'] === 0) {
+                // timeouth=
+            }
+        }
+    } elseif (0) {//BURN IT UP ================================= 0 = not.
+        //This one gets cpu hot.
+        //microtime is slower. fix use it explode by space and make its time boundery use the unix ts plus the mtime for the measure. if a resonable delay was made allow it to recall it self.
+        //		$MEthod='microtime';
+        $MEthod='time';
+        $end = time();
+        //		$diff=round($end,2)-round($Start,2);
+        $diff=$end-$Start;
+        //		die('sleepy:'.$end.'-'.$Start.'='.$diff.'>?'.$timeout.' But the fiish is '.$finishStart);
+        //		if( floatval( (string) $a ) < floatval( (string) $b) ) { //do something }
+        //		$diff=bcsub($end, $Start, 4); //nobcmath
+        while(
+            ($diff < $timeout)
+            ) {
+                //			error_log('sleepy:'.$end.'-'.$Start.'='.$diff.'>?'.$timeout.' But the fiish is '.$finishStart,0);
+                $end = time();
+                //			$diff=round($end,2)-round($Start,2);
+                $diff=$end-$Start;
+                continue;
+            }
+    } else {//fail.
+        return array('ME'=>false);
+    }
+    $end = time();
+    //	while (($end-$Start)<$timeout) skippy_sleep(1);
+    $diff=$end-$Start;
+    return array('Begin'=>$Start,'End'=>$end,'ME'=>$MEthod,'Diff'=>$diff);
+}
+?>

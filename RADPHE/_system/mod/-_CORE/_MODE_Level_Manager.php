6127ef6212be220e97bb38c4315efdf9301f47c0
Kelly G. Allen <KellyGAllen@GMail.com>
2021-02-17 18:11:16 -0800

    _MODE_Before_Level_Manager.php is also called auto called if it is present and then _LVL_ ones for that mod are not. unless it is CORE. _MODE_Level_Manager.php is for you to use manualy. It may also be known as Manual vs Automatic. I suppose that could be a refacotr.

--- RADPHE/_system/mod/-_CORE/_MODE_Level_Manager.php
+++ RADPHE/_system/mod/-_CORE/_MODE_Level_Manager.php
@@ -7,19 +7,24 @@ foreach($_INTIN['MOD']['CORE'] as $_INTIN['CORE Level'] => $_INTIN['CORE']) {
 	$_INTIN['CORE'] = realpath($_INTIN['CORE']);
 	preg_match('/_LVL_(?<LVL>\d{1,4})_?(?<subLVL>\d{1,2})?_(?<LEVEL>.*?).php$/', $_INTIN['CORE'], $_INTIN['RUN']);
 	if ((1)||(!empty($_INTIN['RUN']['LEVEL']))) foreach(glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_LVL_*_'.$_INTIN['RUN']['LEVEL'].'.php') as $_INTIN['MOD Level'] => $_INTIN['CoreMOD']) {
-			$_INTIN['CoreMOD'] = realpath($_INTIN['CoreMOD']);
-			preg_match('/_LVL_(?<LVL>\d{1,4})_?(?<subLVL>[\d\w]{1,2})?_(?<LEVEL>.*?).php$/', $_INTIN['CoreMOD'], $_INTIN['MODRUN']);
-	}
-	if (@$_INTIN['MODRUN']['subLVL']=='before') {
-		bench('CORE#'.$_INTIN['CORE Level'].' MOD#'.$_INTIN['MOD Level'].': '.$_INTIN['MODRUN']['LEVEL'].' as '.$_INTIN['CoreMOD']);
-		if (filesize($_INTIN['CoreMOD'])>10) include_once($_INTIN['CoreMOD']);
-		bench('CORE '.$_INTIN['CORE Level'].': '.$_INTIN['CORE'].'');
-		if (filesize($_INTIN['CORE'])>10) include_once($_INTIN['CORE']);
-	} else {
-		bench('CORE '.$_INTIN['CORE Level'].': '.$_INTIN['CORE'].'');
-		if (filesize($_INTIN['CORE'])>10) include_once($_INTIN['CORE']);
-		bench('CORE#'.$_INTIN['CORE Level'].' MOD#'.$_INTIN['MOD Level'].': '.$_INTIN['MODRUN']['LEVEL'].' as '.$_INTIN['CoreMOD']);
-		if (filesize($_INTIN['CoreMOD'])>10) include_once($_INTIN['CoreMOD']);
+		$_INTIN['CoreMOD'] = realpath($_INTIN['CoreMOD']);
+		preg_match('/(?<file>_LVL_(?<LVL>\d{1,4})_?(?<subLVL>[\d\w]{1,2})?_(?<LEVEL>.*?).php)$/', $_INTIN['CoreMOD'], $_INTIN['MODRUN']);
+		$_INTIN['MODRUN']['manager']=realpath(dirname($_INTIN['CoreMOD']).'_MODE_Before_Level_Manager.php');
+		if (file_exists($_INTIN['MODRUN']['manager'])) {
+			if (filesize($_INTIN['CoreMOD'])>10) include_once($_INTIN['MODRUN']['manager']);
+		} else {//THIS IS THE AFTER MANAGER AS WELL AS DURING unless it is done first.
+			if (@$_INTIN['MODRUN']['subLVL']=='before') {
+				bench('CORE#'.$_INTIN['CORE Level'].' MOD#'.$_INTIN['MOD Level'].': '.$_INTIN['MODRUN']['LEVEL'].' as '.$_INTIN['CoreMOD']);
+				if (filesize($_INTIN['CoreMOD'])>10) include_once($_INTIN['CoreMOD']);
+				bench('CORE '.$_INTIN['CORE Level'].': '.$_INTIN['CORE'].'');
+				if (filesize($_INTIN['CORE'])>10) include_once($_INTIN['CORE']);
+			} else {
+				bench('CORE '.$_INTIN['CORE Level'].': '.$_INTIN['CORE'].'');
+				if (filesize($_INTIN['CORE'])>10) include_once($_INTIN['CORE']);
+				bench('CORE#'.$_INTIN['CORE Level'].' MOD#'.$_INTIN['MOD Level'].': '.$_INTIN['MODRUN']['LEVEL'].' as '.$_INTIN['CoreMOD']);
+				if (filesize($_INTIN['CoreMOD'])>10) include_once($_INTIN['CoreMOD']);
+			}
+		}
 	}
 }
 //support priority #_ mod

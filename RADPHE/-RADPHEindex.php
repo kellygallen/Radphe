e6d0dce3048dcb8df1e69296b26d33ba53e4edd9
Kelly G. Allen <KellyGAllen@GMail.com>
2021-03-02 14:28:52 -0800

    JetBrains PhpStorm - Has Spell Checking. This is good. Because I type phonetically, offloaded to my fingers.

--- RADPHE/-RADPHEindex.php
+++ RADPHE/-RADPHEindex.php
@@ -1,12 +1,12 @@
-<?php @require_once($_SERVER['DOCUMENT_ROOT'].'/_system/_SiteEngine.php'); RadpheFallBackHook;
+<?php @require_once($_SERVER['DOCUMENT_ROOT'].'/_system/_SiteEngine.php'); eval(RadpheFallBackHook);
 //Old way.
 	$_INTIN['Dump'][]='GLOBALS'; //wont work
 	$_INTIN['Dump'][]='_SESSION'; //wont work
 //New Way.
-	$_INTIN['Dump'][]=&$_INTIN['MOD']; //new way. by referance [can change after you referance still linked]
+	$_INTIN['Dump'][]=&$_INTIN['MOD']; //new way. by reference [can change after you reference still linked]
 	$_INTIN['Dump'][]=$_INTIN['CORE']; //new way by copy state.
 //Show This.
-	@$_INTIN['MOD']['CMS']['Blocks']['SupplementryContent'] .= 'Looks like $_INTIN["Dump"][]= is only for passing copies or by referance for now.';
+	@$_INTIN['MOD']['CMS']['Blocks']['SupplementryContent'] .= 'Looks like $_INTIN["Dump"][]= is only for passing copies or by references for now.';
 	$_INTIN['MOD']['CMS']['Blocks']['SupplementryContent'] .= '<blockquote><pre><code>';
 	$_INTIN['MOD']['CMS']['Blocks']['SupplementryContent'] .= highlight_file(__FILE__,true);
 	$_INTIN['MOD']['CMS']['Blocks']['SupplementryContent'] .= '</code></pre></blockquote>';

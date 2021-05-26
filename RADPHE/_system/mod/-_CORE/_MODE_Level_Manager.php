af3a989ac02d20dbbed0e7ef1bab7dfa6de210cc
Kelly G. Allen <KellyGAllen@GMail.COM>
2021-05-26 14:12:51 -0700

    kernel revamp, no testing, sorta there.

--- RADPHE/_system/mod/-_CORE/_MODE_Level_Manager.php
+++ RADPHE/_system/mod/-_CORE/_MODE_Level_Manager.php
@@ -1,39 +1,80 @@
 <?php
+
 global $_INTIN;
 
-/* psudoprocrss build kernel print
-//find all befores use them to make place holder before events in core.
-$_INTIN['KERNEL']['MODULES']['CORE'] //just making this key means core files will be first.
-//Make all CORE, then make all rest.
-$_INTIN['KERNEL']['MODULES'][$MOD]
-KERNEL
-    MOD
-        EVENTS
-            BEFORE event lvl files
-            NATSORTED LVL Files with befores removed or omitted [moved to front].
-*/
-//$_INTIN['KERNEL']['MODfiles']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/*.php');
-$_INTIN['KERNEL']['FILES']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_LVL_*_*.php');
 $_INTIN['KERNEL']['MANAGERS']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_MODE_Level_Manager.php');
-//do just core events... toggle point. or all
-//and only make event names = array();
+foreach($_INTIN['KERNEL']['MANAGERS'] as $Kernel_CORE_Orden => $Kernel_Level) {
+    $_INTIN['KERNEL']['MANAGERS'][$Kernel_CORE_Orden] = realpath($Kernel_Level);
+}
+
+$_INTIN['KERNEL']['CORE']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/-_CORE/_LVL_*.php');
+foreach($_INTIN['KERNEL']['CORE'] as $Kernel_CORE_Orden => $Kernel_Level) {
+    $_INTIN['KERNEL']['FILES'][$Kernel_CORE_Orden] = realpath($Kernel_Level);
+    preg_match('/_LVL_(?<LVL>\d{1,4})_?(?<subLVL>[.\dazA-Z]{1,10})?_(?<LEVEL>.*).php$/', $_INTIN['KERNEL']['CORE'][$Kernel_CORE_Orden], $_INTIN['KERNEL']['EVENTSdetected'][$Kernel_CORE_Orden]);
+    $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$Kernel_CORE_Orden]['LEVEL']]=array();
+}
+
 $_INTIN['KERNEL']['BEFOREs']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_LVL_*_before_*.php');
 foreach($_INTIN['KERNEL']['BEFOREs'] as $Before_Orden => $Kernel_Level) {
     $_INTIN['KERNEL']['BEFORE'][$Before_Orden] = realpath($Kernel_Level);
     preg_match('/_LVL_(?<LVL>\d{1,4})(_before_){1}(?<LEVEL>.*).php$/i', $_INTIN['KERNEL']['BEFORE'][$Before_Orden], $_INTIN['KERNEL']['EVENTSdetected'][$Before_Orden]);
     //make path relative site root or site engine | which is right or full... as is. or include path option.
-    $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$Before_Orden]['LEVEL']][]=$_INTIN['KERNEL']['BEFORE'][$Before_Orden];
+    $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$Before_Orden]['LEVEL']][]=realpath($Kernel_Level);
 }
 
+$_INTIN['KERNEL']['FILES']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_LVL_*_*.php');
 foreach($_INTIN['KERNEL']['FILES'] as $Kernel_Level_Orden => $Kernel_Level) {
     $_INTIN['KERNEL']['FILES'][$Kernel_Level_Orden] = realpath($Kernel_Level);
-    preg_match('/_LVL_(?<LVL>\d{1,4})_?(?<subLVL>[.\dazA-Z]{1,10})?_(?<LEVEL>.*).php$/', $_INTIN['KERNEL']['FILES'][$Kernel_Level_Orden], $_INTIN['KERNEL']['EVENTSdetected'][$Kernel_Level_Orden]);
-    $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$Kernel_Level_Orden]['LEVEL']][]=$Kernel_Level;
+    preg_match('/_LVL_(?<LVL>\d{1,4})_?(?<subLVL>\d{1,4})?_(?<LEVEL>.*).php$/', $_INTIN['KERNEL']['FILES'][$Kernel_Level_Orden], $_INTIN['KERNEL']['EVENTSdetected'][$Kernel_Level_Orden]);
+    if (stripos($Kernel_Level,'_before_')==FALSE)
+        if (stripos($Kernel_Level,'_after_')==FALSE)
+            $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$Kernel_Level_Orden]['LEVEL']][]=realpath($Kernel_Level);
 }
+
+$_INTIN['KERNEL']['AFTERs']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/*/_LVL_*_after_*.php');
+foreach($_INTIN['KERNEL']['AFTERs'] as $After_Orden => $Kernel_Level) {
+    $_INTIN['KERNEL']['AFTER'][$After_Orden] = realpath($Kernel_Level);
+    preg_match('/_LVL_(?<LVL>\d{1,4})(_after_){1}(?<LEVEL>.*).php$/i', $_INTIN['KERNEL']['AFTER'][$After_Orden], $_INTIN['KERNEL']['EVENTSdetected'][$After_Orden]);
+    //make path relative site root or site engine | which is right or full... as is. or include path option.
+    $_INTIN['KERNEL']['EVENTS'][$_INTIN['KERNEL']['EVENTSdetected'][$After_Orden]['LEVEL']][]=realpath($Kernel_Level);
+}
+$_INTIN['KERNEL'] = array('EVENTS'=>$_INTIN['KERNEL']['EVENTS'],'MANAGERS'=>$_INTIN['KERNEL']['MANAGERS']);
 $_INTIN['Dump'][]=$_INTIN['KERNEL'];
 
-
 bench('CORE');
+
+$lasttime=array();
+foreach ($_INTIN['KERNEL']['EVENTS'] as $_INTIN['KERNEL']['EVENT'] => $_INTIN['KERNEL']['EVENTlevel']) {
+    bench(strtoupper('BEGIN '.$_INTIN['KERNEL']['EVENT'].''));
+    foreach ($_INTIN['KERNEL']['EVENTlevel'] as $_INTIN['KERNEL']['EVENTlevelORDEN'] => $_INTIN['KERNEL']['EVENTlevelFILE']) {
+        if (filesize($_INTIN['KERNEL']['EVENTlevelFILE'])>2) {
+            if (!in_array($_INTIN['KERNEL']['EVENTlevelFILE'],get_included_files())) {
+//                if (!empty($lasttime)) $_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][@$_INTIN['KERNEL']['EVENTlevelFILE']]['Prior-STATE'] =$lasttime;
+                bench('BEGIN '.$_INTIN['KERNEL']['EVENT'].''.$_INTIN['KERNEL']['EVENTlevelORDEN']);
+                $_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]		['ACL'] = include_once($_INTIN['KERNEL']['EVENTlevelFILE']);
+
+//                    array_key_last($_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]	['RETURN']);
+
+
+ /*               if (!empty($_INTIN['Dump'])) {
+                    $_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]['Prior-STATE'] = @array_diff($_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]['Prior-STATE'],$alltime);
+                } else $_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]['Prior-STATE'] = 'NO INTIN DUMP yet.'; */
+                if (!isset($_INTIN['MOD']['CMS']['Blocks']))
+                    $_INTIN['MOD']['CMS']['Blocks']=array();
+                if (!isset($alltime))
+                    $alltime=array();
+                $_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['KERNEL']['EVENT']][$_INTIN['KERNEL']['EVENTlevelORDEN']][$_INTIN['KERNEL']['EVENTlevelFILE']]['BLOCK-Changes'] = @array_diff($_INTIN['MOD']['CMS']['Blocks'],$alltime);
+                //$_INTIN['MOD']['']['AWARENESS']['Mods'][$_INTIN['CORE Level']][@$_INTIN['MOD Level']][$_INTIN['CORE']][CORE'.$_INTIN['CORE Level'].' Relativity'.$_INTIN['RUN']['subLVL'].' MOD'.@$_INTIN['MOD Level'].' Priority'.$_INTIN['MODRUN']['LVL'].' Relativity'.$_INTIN['MODRUN']['subLVL']]['BLOCK-Changes'] = @array_diff($_INTIN['MOD']['CMS']['Blocks'],$alltime);
+                $lasttime=@array_diff($_INTIN['MOD']['CMS']['Blocks'],$alltime);
+                $alltime=$_INTIN['MOD']['CMS']['Blocks'];
+            }
+        }
+    }
+}
+
+return true;
+
+
 $_INTIN['MOD']['CORE']=glob($_SERVER['DOCUMENT_ROOT'].'/_system/mod/-_CORE/_LVL_*_*.php');
 $lasttime=array();
 //$alltime=array();

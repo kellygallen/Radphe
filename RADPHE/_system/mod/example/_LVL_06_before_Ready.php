6b288c7f37bde8737e591fde74b091cf17de69e8
Kelly G. Allen <KellyGAllen@GMail.COM>
2021-06-01 19:48:52 -0700

    ..

--- RADPHE/_system/mod/example/_LVL_06_before_Ready.php
+++ RADPHE/_system/mod/example/_LVL_06_before_Ready.php
@@ -1,9 +1,11 @@
 <?php 
     if (($_SERVER['REQUEST_URI']==$_SERVER['PHP_SELF'])||($_SERVER['REQUEST_URI']=='/'.$_SERVER['PHP_SELF'])) {
-        @$_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '';
-        $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '<center><hr>Contents of this <b>REAL SITE ROOT FILE</b>: /'.$_SERVER['PHP_SELF'].'<hr></center>';
-        $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '<blockquote><pre><code>';
-        $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= highlight_file($_SERVER['DOCUMENT_ROOT'].'/'.$_SERVER['PHP_SELF'],true);
-        $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '</code></pre></blockquote>';
+        if (file_exists($_SERVER['DOCUMENT_ROOT'].'/'.$_SERVER['PHP_SELF'])) {
+            @$_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '';
+            $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '<center><hr>Contents of this <b>REAL SITE ROOT FILE</b>: /'.$_SERVER['PHP_SELF'].'<hr></center>';
+            $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '<blockquote><pre><code>';
+            $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= highlight_file($_SERVER['DOCUMENT_ROOT'].'/'.$_SERVER['PHP_SELF'],true);
+            $_INTIN['MOD']['CMS']['Blocks']['SupplementaryContent'] .= '</code></pre></blockquote>';
+        }
     }
 ?>
